e2305f1f0a9424db76d450991d9478c849db333b
hailey-tam-ics4u1-03-25 <hailey.tam@student.tdsb.on.ca>
2025-04-09 00:18:55 +0000

    Completed Code

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
 
+	public static ArrayList<String> perms(String str){
+		ArrayList<String> perms = new ArrayList<String>();
+		permsHelper("", str, perms);
+		return perms;
+	}
+
+	public static void permsHelper(String usedLetters, String unusedLetters, ArrayList<String> perms){
+
+		//base case
+		if (unusedLetters.length() == 0){
+			perms.add(usedLetters);
+		}
+
+
+		//recursive case
+		for (int i = 0; i < unusedLetters.length(); i++){
+			char currentChar = unusedLetters.charAt(i);
+			String used = usedLetters + currentChar;
+			String unused = unusedLetters.substring(0, i) + unusedLetters.substring(i + 1);
+			permsHelper(used, unused, perms);
+		}
+	}
+
+	public static ArrayList<String> permsUnique(String str){
+		ArrayList<String> perms = new ArrayList<String>();
+
+		permsUniqueHelper("", str, perms);
+		return perms;
+	}
+
+
+	public static void permsUniqueHelper(String usedLetters, String unusedLetters, ArrayList<String> perms){
+
+		//base case
+		if (unusedLetters.length() == 0){
+			if (!perms.contains(usedLetters)){
+				perms.add(usedLetters);
+			}
+		}
+	
+		//recursive case
+		for (int i = 0; i < unusedLetters.length(); i++){
+			char currentChar = unusedLetters.charAt(i);
+			String used = usedLetters + currentChar;
+			String unused = unusedLetters.substring(0, i) + unusedLetters.substring(i + 1);
+			permsUniqueHelper(used, unused, perms);
+		}
+	}
 }
